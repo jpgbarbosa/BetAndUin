5aa98186846682e565bef1812b75dda931a1884a
Joao Barbosa <j.barbosa333@gmail.com>
2010-10-15 19:32:26 +0100

    We need to save the messages to file. Not tested yet.

--- BetAndUin/src/clientRMI/RMIWriter.java
+++ BetAndUin/src/clientRMI/RMIWriter.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.rmi.RemoteException;
 import java.util.Vector;
 
+import constants.Constants;
+
+import server.ClientOperations;
 import server.Server;
 
 import clientTCP.ConnectionLock;
@@ -23,6 +26,7 @@ public class RMIWriter extends Thread{
 	boolean debugging = true;
 	
 	String userInput, serverAnswer, username;
+	String [] stringSplitted;
 	
 	RMIWriter(ConnectionLock connectionLock, RMIClient client){
 		this.connectionLock = connectionLock;
@@ -38,31 +42,156 @@ public class RMIWriter extends Thread{
     				connectionLock.wait();
 				} catch (InterruptedException e) {
 					if (debugging){
-						System.out.println("ClientWriteTCP Thread interrupted.");
+						System.out.println("ClientWriteRMI Thread interrupted.");
 					}
 				}
     		}
     	}
 		
         while(true){
-        	try{
-            	userInput = reader.readLine();
+        	try{ 	
+    	    	stringSplitted = null;
+    	    	userInput =  reader.readLine();
+    	    	
+    	    	/* Splits the input. TODO: é suposto proteger split com algum try catch?*/
+    	    	stringSplitted = userInput.split(" ");
             	
-            	if(connectionLock.isConnectionDown() && userInput.split(" ")[0].equals("send")){
-            		//grava cenas em ficheiro
-            	} else if(!connectionLock.isConnectionDown()){
-					System.out.println("\n>> ");
-					serverAnswer = client.parseFunction(username, userInput, server, reader);
-					System.out.println(serverAnswer);
+            	synchronized(connectionLock){
+	            	if(connectionLock.isConnectionDown() && stringSplitted[0].equals("send")){
+	            		//grava cenas em ficheiro
+	            	} else if(!connectionLock.isConnectionDown()){
+						System.out.println("\n>> ");
+						serverAnswer = parseFunction(username, stringSplitted, userInput, server, reader);
+						System.out.println(serverAnswer);
+	            	}
             	}
             }catch(RemoteException e){
             	//grava em ficheiro o comando userInput;
+            	
+            	/*Se estoirou, significa que temos de guardar este ultimo comando
+            	* visto que só estoira qd efectuamos o comando, certo?
+            	*/
+            	connectionLock.setConnectionDown(true);
             	connectionLock.notify();
 	        } catch (IOException e) {
 				e.printStackTrace();
 			}
         }
 	}
+	
+	   public String parseFunction(String user, String [] stringSplitted, String input, ClientOperations server, BufferedReader reader) throws RemoteException{
+	    	/* The answer from the server to the client. */
+	    	String answer = "";
+
+	        
+	        /* The client has sent two keywords and the first is "show".*/
+	        if(stringSplitted.length == 2 && stringSplitted[0].equals("show")){        	
+	        	if(stringSplitted[1].equals("matches")){ //show all current matches
+	        		answer = server.clientShowMatches();
+	        	}
+	        	else if(stringSplitted[1].equals("credits")){ //show user's credits
+	        		answer = server.clientShowCredits(user);
+	        	}
+	        	else if(stringSplitted[1].equals("users")){ //show all active users
+	        		answer = server.clientShowUsers();
+	        	}
+	        	else if(stringSplitted[1].equals("menu")){//show the main menu.
+	        		answer = server.clientShowMenu();
+	        	}
+	        	else{
+	        		answer = "Unknow Command";
+	        	}
+	        }
+	        else if(stringSplitted.length >= 3 && stringSplitted[0].equals("send")){
+	        	if(stringSplitted[1].equals("all")){ //send a message to all users
+	        		/* We have to use again the input because the user may send the message with
+	        		 * white spaces.
+	        		 * 'send all ' has 9 characters. So, the message is from there till the end of
+	        		 * input string.
+	        		 */
+	        		answer = server.clientSendMsgAll(user, input.substring(9));
+	        	}
+	        	else if (stringSplitted[1].equals(user)){
+	        		answer = "What's the point of sending messages to yourself?";
+	        	}
+	        	/* We are sending a message to a user. */
+	        	else{
+	        		/* 'send ' has 5 characters. Then, we have to sum the size of the receiver name
+	        		 * as well as a white space that separates this name from the message.
+	        		 */
+	        		answer = server.clientSendMsgUser(user, stringSplitted[1],input.substring(6 + stringSplitted[1].length()));
+	        	}
+	        }
+	        else if(stringSplitted.length == 4 && stringSplitted[0].equals("bet")){
+	        	/* Variables to save the values inserted by the client. */
+	        	String resultBet;
+	        	int gameNumber,credits;
+	        	
+	        	try {
+	        		gameNumber = Integer.parseInt(stringSplitted[1]);
+	        		resultBet = stringSplitted[2];
+	        	    credits = Integer.parseInt(stringSplitted[3]);
+	        	    
+	        	} catch(NumberFormatException nFE) {
+	        		/* The user hasn't inserted a number for one of the required arguments. */
+	        		System.out.println("Not an integer.");
+	        		
+	        		answer = "Invalid game number or amount of credits!";
+	        	    return answer;
+	        	}
+	        	
+	        	/* The user has made a bet of 0 credits. */
+	        	if (credits == 0){
+	        		return "Are you kidding?! You have bet no credits!";
+	        	}
+	        	
+	        	answer = server.clientMakeBet(user,gameNumber, resultBet, credits);
+	        }
+	        else if(stringSplitted.length > 0 && stringSplitted[0].equals("bet")){
+	        	answer =  "Wrong number of arguments: bet [game number] [bet] [credits]";
+	        }
+	        else if (input.equals("reset")){
+	        	/* First, we verify if the client has more or less than the default value of credits,
+	        	 * make sure he/she won't lose credits accidentally.
+	        	 */
+	    		int userCredits = Integer.parseInt(server.clientShowCredits(user));
+
+	    		if (userCredits > Constants.DEFAULT_CREDITS){
+	    			String finalAnswer = "";
+	    			System.out.printf("In this moment, you have %d, which means you are going to lose %d credits.\n" +
+	    					"Are you sure you want to continue with the process (Y/N)?\n", userCredits, userCredits - Constants.DEFAULT_CREDITS);
+	    			do{
+	    				try{
+	    					finalAnswer = reader.readLine().toUpperCase();
+	    				}catch (Exception e){
+	    					return "";
+	    				}
+	    			}
+	    			while (!finalAnswer.equals("Y") && !finalAnswer.equals("N"));
+	    			
+	    			if (finalAnswer.equals("Y")){
+	    				answer = server.clientResetCredits(user);
+	    			}
+	    			else{
+	    				answer = "Operation cancelled. You still have " + userCredits+ " credits.\n";
+	    			}
+	        	}
+	    		else{
+	    			answer = server.clientResetCredits(user);
+	    		}
+	        }
+	        else if(stringSplitted.length == 1 && stringSplitted[0].equals("exit")){
+	        	server.clientLeave(user);
+	        	System.out.println("Thank you for using the BetAndUin service!\n"
+	    				+ "Have a nice day!");
+	    		System.exit(0);
+	        }
+	        else {
+	        	answer = "Unknown command";
+	        }
+
+			return answer;
+	    }
 	
 	protected void setClient(RMIClient client){
 		this.client = client;
