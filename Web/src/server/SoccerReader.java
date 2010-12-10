ad3743f624c38c150c0f733e0bd1184d85215a15
icorreia <icorreia@student.dei.uc.pt>
2010-12-10 00:41:18 +0000

    A versao mais limpa de todas

--- Web/src/server/SoccerReader.java
+++ Web/src/server/SoccerReader.java
@@ -3,7 +3,6 @@ package server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
